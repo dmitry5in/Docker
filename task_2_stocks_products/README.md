a0a4571320d54cf1e27bf6bf21236e67a1948816
Dmitry Pyatin <dmitry005@gmail.com>
2022-03-10 21:03:22 +0500

    first commit

---
+++ task_2_stocks_products/README.md
@@ -0,0 +1,3 @@
+# Создание образа и запуск
+* docker build -t <image_name> <directory> 
+* docker run -p 8000:8000 <image_name>
---
+++ task_2_stocks_products/logistic/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'stocks']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().create(validated_data)
+
+        for i in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                product=i.get('product'),
+                quantity=i.get('quantity'),
+                price=i.get('price'),
+            )
+
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().update(instance, validated_data)
+
+        for i in positions:
+            StockProduct.objects.update_or_create(
+                product=i.get('product'),
+                stock=stock,
+                defaults={
+                    'quantity': i.get('quantity'),
+                    'price': i.get('price'),
+                }
+            )
+
+        return stock
